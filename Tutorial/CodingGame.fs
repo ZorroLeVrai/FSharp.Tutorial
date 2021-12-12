0ff9d610846df16c20b9f77046826b8e20f581e9
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2021-12-12 23:58:15 +0100

    Adding Coding Game helper and ShiftString

---
+++ Tutorial/CodingGame.fs
@@ -0,0 +1,46 @@
+﻿module CodingGame
+
+(* HANDLING INPUTS *)
+
+let captureValuesWithArray inputSequence =
+    let output = Array.create (Seq.length inputSequence) ""
+    let mutable index = 0
+    for input in inputSequence do
+        output.[index] <- input
+        index <- index + 1
+    output
+
+let captureValuesWithList inputSequence =
+    let mutable output = []
+    for input in inputSequence do
+        output <- input::output
+    output
+    |> List.rev
+    |> List.toArray
+
+let captureValuesCompact inputSequence =
+    inputSequence
+    |> Seq.fold (fun acc item -> item::acc) []
+    |> List.rev
+
+(* CONVERTING ASCII TO STRING *)
+let asciiToString (byteArr: byte array) =
+    System.Text.ASCIIEncoding.ASCII.GetString(byteArr)
+
+let stringToAscii (str: string) =
+    System.Text.ASCIIEncoding.ASCII.GetBytes(str)
+
+(* CONVERTING STRING TO ARRAY OF CHAR *)
+let toCharArray str =
+    str
+    |> Seq.toArray
+
+let toString (charArr: char array) =
+    System.String.Concat(charArr)
+
+(* RETURNING THE LAST ITEM OF A LIST *)
+let rec getLastItem lst =
+    match lst with
+    | [x] -> x
+    | x::xs -> getLastItem xs
+    | _ -> failwith "Empty list"
---
+++ Tutorial/ShiftString.fs
@@ -0,0 +1,26 @@
+﻿module ShiftString
+(* Shift a string either to the left or to the right *)
+
+let leftShift = function
+    | [] -> []
+    | x::xs -> xs @ [x]
+
+let rightShift lst =
+    lst
+    |> List.rev
+    |> leftShift
+    |> List.rev
+
+let completeShift (shiftFunc: char list -> char list) (str:string) =
+    let charArr =
+        str
+        |> Seq.toList
+        |> shiftFunc
+        |> List.toArray
+    System.String.Concat(charArr)
+
+let change str command =
+    match command with
+    | "L" -> completeShift leftShift str
+    | "R" -> completeShift rightShift str
+    | _ -> failwith "Invalid command"
